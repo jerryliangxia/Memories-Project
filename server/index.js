8701977f28c50466d5f1d7dc94912ddc2b231391
Jerry Xia <jerrylxia@gmail.com>
2022-02-02 23:04:31 -0500

    first stable build

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,19 +15,22 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb://jerrylxia:jerrylxia123@cluster0-shard-00-00.ryiwt.mongodb.net:27017,cluster0-shard-00-01.ryiwt.mongodb.net:27017,cluster0-shard-00-02.ryiwt.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zz4ytf-shard-0&authSource=admin&retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
+
 // helped from: https://stackoverflow.com/questions/55499175/how-to-fix-error-querysrv-erefused-when-connecting-to-mongodb-atlas
 // (changed node to version 2.2.12)
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
   )
   .catch((error) => console.log(`${error} did not connect`));
 
