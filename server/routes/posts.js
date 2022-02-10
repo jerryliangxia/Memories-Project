9e352ddd69fde1f045d2ea0162d50ac2abbada19
Jerry Xia <jerrylxia@gmail.com>
2022-02-10 15:35:10 -0500

    working individual post display

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -12,8 +12,8 @@ import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/:id", getPost);
 router.get("/search", getPostsBySearch);
+router.get("/:id", getPost);
 router.get("/", getPosts);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
